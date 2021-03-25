39e79d934a5df563034e77666fff2d0b403e6caf
jurra <jose.urra86@gmail.com>
2021-03-25 17:23:22 +0100

    Improved code

--- 03_notebook/notebook.js
+++ 03_notebook/notebook.js
@@ -4,21 +4,30 @@ let listeners = [];
 class Cell {
     constructor(cells) {
       if (cells.length) { this.id = cells.length } else { this.id = 0 }
-      this.content = 'console.log("Hello world")';
+      this.content = '';
       this.isEditing = true // Substitutes the element and puts a form
       this.template = `
-      <div>
+      <div id="cell__`+ this.id +`">
         <form style="padding:1rem 1rem 0rem 0rem">
             <textarea 
                 class="code"
-                id="cell__`+ this.id +`" 
+                id="cell__text__`+ this.id +`" 
                 name="" rows="4" cols="50" 
                 style="width:100%;align-content:center; overflow:auto;">
             `+ this.content +`
             </textarea>
             <br><br>
          </form>
-        <button class="mdc-button mdc-button--outlined"onclick="run(cell__`+ this.id +`)">Run</button>
+        <button class="mdc-button mdc-button--outlined"
+            onclick="run(cell__text__`+ this.id +`)"
+        >
+            Run
+        </button>
+        <button class="mdc-button mdc-button--outlined"
+            onclick="removeCell('cell__`+ this.id + `')"
+        >
+            Delete
+        </button>
         <p></p>
         </div>
         `
@@ -37,5 +46,10 @@ function run(cell){
     console.log("Working fine " + cell.id);
     let code = document.getElementById(cell.id).value;
     eval(code)
+}
+
+function removeCell(id){
+    let cell_element = document.getElementById(id)
+    cell_element.remove()
 }
   
