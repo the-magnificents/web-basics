96f560d752ad7acea6acccc8292cb5c1c9554deb
jurra <jose.urra86@gmail.com>
2021-03-25 16:51:52 +0100

    Succesfully runs code in the console

--- 03_notebook/notebook.js
+++ 03_notebook/notebook.js
@@ -1,51 +1,42 @@
-let cells = []
-let listeners = []
+let cells = [];
+let listeners = [];
 
 class Cell {
     constructor(cells) {
       if (cells.length) { this.id = cells.length } else { this.id = 0 }
-      this.content = 'console.log("Hello World")';
+      this.content = 'console.log("Hello world")';
       this.isEditing = true // Substitutes the element and puts a form
       this.template = `
+      <div>
         <form style="padding:1rem 1rem 0rem 0rem">
-            <textarea id="`+ this.id +`" name="" rows="4" cols="50" style="text-align:left;">
+            <textarea 
+                id="cell__`+ this.id +`" 
+                name="" rows="4" cols="50" 
+                style="width:100%;align-content:center; overflow:auto;">
             `+ this.content +`
             </textarea>
             <br><br>
          </form>
-        <button id=add__`+ this.id +` onclick="run(add__`+ this.id +`)">Run</button>
-      `
-    //   this.click = document.getElementById('add__' + this.id)
-    //   .addEventListener('click',function(){
-    //       console.log("Testing event listener in: cell " + this.id  )
-    //    });
-    }
-
-    run(){
-        console.log("Cell responding")
-        eval(this.content)
-    }
-
-
-    setId(id){
-        this.id = id
+        <button class="mdc-button mdc-button--outlined"onclick="run(cell__`+ this.id +`)">Run</button>
+        <p></p>
+        </div>
+        `
     }
   }
 
-// function makeCellTemplate(cell=""){
-    
-// }
-
 function addCell(elementId){
-    cell = new Cell(cells)
-    cells.push(cell)
+    cell = new Cell(cells);
+    cells.push(cell);
     element = document.getElementById(elementId)
-    element.insertAdjacentHTML('beforebegin', cell.template)
-    console.log('cellId is ' + cell.id)
+    element.insertAdjacentHTML('beforebegin', cell.template);
+    console.log('cellId is ' + cell.id);
 }
 
 function run(cell){
-    console.log("Working fine " + cell.id)
-    // cells[cell.id].run()
+    console.log("Working fine " + cell.id);
+    let code = document.getElementById(cell.id).value;
+    function getOutput(){
+        return Function('"use strict"; return('+ code + ')')();
+    }
 }
   
